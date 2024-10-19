f39db57fda3c95d435d6e26caeb321f1019ee43f
yazgiakata <gullerya@buffalo.edu>
2024-10-19 15:16:57 -0400

    committing

---
+++ todo/views.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+from django.views.generic import ListView
+from .models import ToDoItem
+
+
+class AllToDos(ListView):
+    model = ToDoItem
+    template_name = "todo/index.html"
+
+
+class TodayToDos(ListView):
+    model = ToDoItem
+    template_name = "todo/today.html"
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(due_date=date.today())
